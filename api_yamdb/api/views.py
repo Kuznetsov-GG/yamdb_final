bd53b9746ffdc74134a6af098cfc98a394f41f01
Григорий Кузнецов <grishik77788@gmail.com>
2022-09-18 11:07:56 +0500

    My first commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Category, Genre, Review, Title, User
 
-
 from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
 from .permissions import AdminOrReadOnly, IsAdmin, UserModeratorAdminOrReadOnly
