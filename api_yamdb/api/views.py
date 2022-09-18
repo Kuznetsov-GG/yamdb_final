9607497314616ab7be657edd29b958949bc1e2ea
Григорий Кузнецов <grishik77788@gmail.com>
2022-09-18 10:49:09 +0500

    My first commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
-
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
@@ -11,6 +10,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
+
 from reviews.models import Category, Genre, Review, Title, User
 from api_yamdb.settings import DEFAULT_FROM_EMAIL
 
