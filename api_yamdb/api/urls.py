67928e637e16865c63d17baab992d1380859cbad
Григорий Кузнецов <grishik77788@gmail.com>
2022-09-18 13:07:25 +0500

    My first commit

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CommentsViewSet,
-                    ReviewsViewSet,
-                    UserViewSet,
-                    SignUp,
-                    GetToken,
-                    CategoryViewSet,
-                    GenreViewSet,
-                    TitleViewSet
-                    )
+from .views import (CategoryViewSet, CommentsViewSet, GenreViewSet, GetToken,
+                    ReviewsViewSet, SignUp, TitleViewSet, UserViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename="users")
