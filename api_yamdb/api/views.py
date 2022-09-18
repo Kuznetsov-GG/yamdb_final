3d5527193b90f5467e578c8dcb0db20d59c098b3
Григорий Кузнецов <grishik77788@gmail.com>
2022-09-18 10:32:25 +0500

    My first commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,6 @@
-from api_yamdb.settings import DEFAULT_FROM_EMAIL
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from api_yamdb.settings import DEFAULT_FROM_EMAIL
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
