f3d036259b45daf11a49e13599cd21b7e4159df8
Григорий Кузнецов <grishik77788@gmail.com>
2022-09-22 22:48:51 +0500

    My first commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = strtobool(os.getenv('DEBUG', default='False'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
 
 
 INSTALLED_APPS = [
